21d2ebb9e2cc086c195ef695da2cdc641910c144
Nicolas Pfeiffer <92082830+pfenic@users.noreply.github.com>
2021-10-15 18:04:17 +0900

    Clean up and basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,66 +2,82 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
-import Footer from "react-bootstrap/ModalFooter";
 import Container from "react-bootstrap/Container";
+import LoginPage from "./components/LoginPage";
 
 // rafce
-          //<Navbar.Collapse id="navmenu"></Navbar.Collapse>
+      //<Container fluid className="p-0 min-vh-100 bg-secondary">
 function App() {
   return (
     <Router>
-      <Navbar variant="dark" bg="dark" expand="sm">
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>Issue Tracker</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav className="ms-auto">
-              <LinkContainer to="/">
-                <Nav.Link>Sign up</Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div style={{ height: "100vh" }}>
+        <Navbar variant="dark" bg="dark" expand="sm" style={{ height: "4rem" }}>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Issue Tracker</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav className="ms-auto">
+                <LinkContainer to="/">
+                  <Nav.Link>Sign up</Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-      <section style={{ minHeight: "70vh" }} className="bg-secondary p-5">
-        <div style={{ height: "70vh" }} className="container">
-          <Route
-            path="/"
-            exact
-            render={(props) => (
-              <>
-                <div className="">
-                  <LinkContainer to="/test">
-                    <Button className="btn-lg btn-danger">Foo</Button>
-                  </LinkContainer>
-                </div>
-              </>
-            )}
-          />
-          <Route
-            path="/test"
-            exact
-            render={(props) => (
-              <LinkContainer to="/">
-                <Button>Foo</Button>
-              </LinkContainer>
-            )}
-          />
+        <div
+          className="bg-secondary p-5"
+          style={{
+            display: "flex",
+            minHeight: "calc(100% - 13rem)",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Route path="/" exact component={LoginPage} />
         </div>
-      </section>
-      <Footer className="bg-dark text-light p-5 text-center">
-        <div className="container">
-          <p className="lead">Copyright &copy; 2021 Nicolas Pfeiffer</p>
-        </div>
-      </Footer>
+
+        <footer
+          className="bg-dark text-light p-5 text-center"
+          style={{ height: "9rem" }}
+        >
+          <Container>
+            <p className="lead">Copyright &copy; 2021 Nicolas Pfeiffer</p>
+          </Container>
+        </footer>
+      </div>
     </Router>
   );
 }
+      //</Container>
 
 // style={{ minHeight: '100vh'}}
+/*
+        <Route
+          path="/"
+          exact
+          render={(props) => (
+            <>
+              <div className="">
+                <LinkContainer to="/test">
+                  <Button className="btn-lg btn-danger">Foo</Button>
+                </LinkContainer>
+              </div>
+            </>
+          )}
+        />
+        <Route
+          path="/test"
+          exact
+          render={(props) => (
+            <LinkContainer to="/">
+              <Button>Foo</Button>
+            </LinkContainer>
+          )}
+        />
+        */
 
 export default App;
