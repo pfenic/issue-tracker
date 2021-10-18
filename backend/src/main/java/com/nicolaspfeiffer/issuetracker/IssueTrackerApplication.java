c44e7f24f2d2fdcff7219a242568310cf8061d13
Nicolas Pfeiffer <92082830+pfenic@users.noreply.github.com>
2021-10-18 12:09:46 +0900

    Rename project to issuetracker

--- backend/src/main/java/com/nicolaspfeiffer/issuetracker/IssueTrackerApplication.java
+++ backend/src/main/java/com/nicolaspfeiffer/issuetracker/IssueTrackerApplication.java
@@ -1,13 +1,13 @@
-package com.nicolaspfeiffer.backend;
+package com.nicolaspfeiffer.issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class IssueTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(IssueTrackerApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/nicolaspfeiffer/issuetracker/IssueTrackerApplicationTests.java
+++ backend/src/test/java/com/nicolaspfeiffer/issuetracker/IssueTrackerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nicolaspfeiffer.backend;
+package com.nicolaspfeiffer.issuetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class IssueTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
