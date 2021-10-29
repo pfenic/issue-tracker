ef39c6f70fbbbc4e6d8923c8c2d76e073d8b1e0a
Nicolas Pfeiffer <92082830+pfenic@users.noreply.github.com>
2021-10-29 14:17:37 +0900

    Fix redux devtool support
    Only support the redux devtool when in development build.

--- src/main/frontend/src/store.js
+++ src/main/frontend/src/store.js
@@ -5,13 +5,16 @@ import rootReducer from "./reducers";
 const initalState = {};
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initalState,
-  compose(
+const enhancer = [
     applyMiddleware(...middleware),
+];
+
+if (process.env.NODE_ENV === "development") {
+  enhancer.push(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+  );
+}
+
+const store = createStore(rootReducer, initalState, compose(...enhancer));
 
 export default store;
